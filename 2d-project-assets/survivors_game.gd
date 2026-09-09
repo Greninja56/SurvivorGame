ae73bf2e51bde4f62efdad725341291929b79096
Thomas Parameswaran <thpa8174@colorado.edu>
2026-09-08 23:41:18 -0600

    added a story!
    added some exposition, kinda, balanced slimes and win timer for more fun

--- 2d-project-assets/survivors_game.gd
+++ 2d-project-assets/survivors_game.gd
@@ -1,10 +1,24 @@
 extends Node2D
 
-var time_left = 120
+var time_left = 60
+
+var story_index = 0
+
+var story_list = [
+	"Kshhhhh. Got-kshhhhhh-hing!",
+	"Soldie-kshh. Kshhhh-ear me!",
+	"You're sur-kshh-nded! Choppe-kshhhhhh-way!",
+	"Survive unt-kshh-e get there!"
+]
 
 func _ready():
+	%StoryLabel.text = story_list[0]
+	%StoryTimer.start()
+	
+func start_game():
 	%Countdown.text = "Survive: " + str(time_left)
 	%WinTimer.start()
+	%Timer.start()
 
 func spawn_mob():
 	var new_mob = preload("res://mob.tscn").instantiate()
@@ -22,10 +36,18 @@ func _on_player_health_depleted() -> void:
 	%GameOver.visible = true
 	get_tree().paused = true
 
-
 func _on_win_timer_timeout() -> void:
 	time_left -= 1
 	%Countdown.text = "Survive: " + str(time_left)
 	if time_left <= 0:
 		%Victory.visible = true
 		get_tree().paused = true
+
+func _on_story_timer_timeout() -> void:
+	story_index += 1
+	if story_index < story_list.size():
+		%StoryLabel.text = story_list[story_index]
+		%StoryTimer.start()
+	else:
+		%StoryLabel.visible = false
+		start_game()
